4b88045b59d649c9003d5c5629525d6b38f30c47
Mundurujoana <mundujoana@com>
2025-11-19 07:08:13 +0200

    Add pointer cursor to OptionCard

--- src/components/OptionCard.tsx
+++ src/components/OptionCard.tsx
@@ -10,11 +10,11 @@ export default function OptionCard({ option }: OptionCardProps) {
   const Icon = option.icon;
 
   return (
-    <div className="flex flex-col items-center p-3 bg-white rounded-xl shadow border border-borderGray">
+    <div className="flex flex-col items-center p-3 bg-white rounded-xl shadow border border-borderGray cursor-pointer">
       <Icon className="w-6 h-6 text-appText mb-2" />
-<p className="text-[10px] text-center text-grayText">
-  {option.title}
-</p>
+      <p className="text-[10px] text-center text-grayText">
+        {option.title}
+      </p>
     </div>
   );
 }
